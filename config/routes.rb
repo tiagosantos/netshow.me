af59b3492a529b6300e3f264298da1693669c381
Carlos Tiago <ontiago@gmail.com>
2019-08-08 10:42:25 -0300

    {USER} autentication routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :movies
   resources :categories
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  authenticate :user do
+	  resources :movies, only: [:new, :create, :edit, :update, :destroy]
+	end
+  resources :movies, only: [:index, :show]
 
   root 'movies#index'
 end
