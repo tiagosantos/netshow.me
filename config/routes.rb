ad52363a7afa2d59d1ef9fe9b45a4777e37996ce
Carlos Tiago <ontiago@gmail.com>
2019-08-08 13:48:47 -0300

    Rails routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   authenticate :user do
-	  resources :movies, only: [:new, :create, :edit, :update, :destroy]
-	end
+	resources :movies, only: [:new, :create, :edit, :update, :destroy]
+  end
   resources :movies, only: [:index, :show]
-
-  root 'movies#index'
+  resources :users, only: [:show]
+  get "/pages/:page" => "pages#show"
+  root 'pages#home'
 end
