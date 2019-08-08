4b5b2ae0e9cf89aaf64e41af04dbe02a539d4b30
Carlos Tiago <ontiago@gmail.com>
2019-08-08 16:15:28 -0300

    {MOVIE} validation

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,8 @@ class Movie < ApplicationRecord
   belongs_to :user
   belongs_to :category
   has_many :views
+
+  validates :user_id, :name, :url, :category_id, presence: true
+  #validates :url, uniqueness: { message: "Essa sala já está reservada para este horário" }
+
 end
